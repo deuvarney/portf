dea317a8e5f361b74c432ec3d7c595ef80c9a654
deuvarney <deuvarney@yahoo.com>
2023-12-19 03:12:56 -0500

    Initial Commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+export default function Home(){
+    return (
+        <>
+        <h1>Welcome to Deuvarney's Online Portfolio</h1>
+        <img alt="Deuvarney's Profile Picture" src="https://placehold.co/600x400"/>
+        <h2>Building Tomorrow&apos;s Solutions, Today.</h2>
+
+        <h3>About Myself</h3>
+        <p>I am a results-driven software engineer with a strong background in [key technologies]. My passion lies in [specific areas of interest], and I am dedicated to delivering high-quality solutions that drive innovation.</p>
+        </>
+    );
+}
