5cdd89f39a05e543a80ea77123dc661772d3a6f4
deuvarney <deuvarney@yahoo.com>
2024-02-20 23:06:59 -0500

    Update Mobile Text Size and LOGO Text

--- src/components/Appbar/index.tsx
+++ src/components/Appbar/index.tsx
@@ -121,7 +121,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            D&apos;s Portfolio
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map(([pageName, pathnamez]) => (
