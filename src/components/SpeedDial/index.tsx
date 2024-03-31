7f5114ab036f742586343618278546cb3f2261f2
deuvarney <deuvarney@yahoo.com>
2024-03-30 21:31:54 -0400

    Fix SpeedDial Btn placement

--- src/components/SpeedDial/index.tsx
+++ src/components/SpeedDial/index.tsx
@@ -1,47 +1,39 @@
 import Box from '@mui/material/Box';
 import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
-import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import PrintIcon from '@mui/icons-material/Print';
-import ShareIcon from '@mui/icons-material/Share';
 
 import './styles.scss';
+import styles from './styles.module.scss';
 import Link from 'next/link';
-import { Email, GitHub, Launch, LinkedIn, Phone } from '@mui/icons-material';
+import { Email, GitHub, Launch, LinkedIn, Phone, Close } from '@mui/icons-material';
 
 
 // TODO, consoldiate contain information
 const actions = [
-//   { icon: <FileCopyIcon />, name: 'Copy' },
-//   { icon: <SaveIcon />, name: 'Save' },
-//   { icon: <PrintIcon />, name: 'Print' },
-//   { icon: <ShareIcon />, name: 'Share' },
   { name: 'Call',
     icon: (
-        <Link target="_blank" rel="noopener noreferrer" href={"tel:11234567890"}>
+        <Link className={styles.speedDialLink} target="_blank" rel="noopener noreferrer" href={"tel:11234567890"}>
             <Phone/>
         </Link>
     )
     },
     { name: 'Email',
     icon: (
-        <Link target="_blank" rel="noopener noreferrer" href={"mailto:deuvarney1@aol.com"}>
+        <Link className={styles.speedDialLink} target="_blank" rel="noopener noreferrer" href={"mailto:deuvarney1@aol.com"}>
             <Email/>
         </Link>
     )
     },
     { name: 'LinkedIn',
     icon: (
-        <Link target="_blank" rel="noopener noreferrer" href={"https://www.linkedin.com/in/deuvarney"}>
+        <Link className={styles.speedDialLink} target="_blank" rel="noopener noreferrer" href={"https://www.linkedin.com/in/deuvarney"}>
             <LinkedIn/>
         </Link>
     )
     },
     { name: 'Github',
     icon: (
-        <Link target="_blank" rel="noopener noreferrer" href={"https://github.com/deuvarney"}>
+        <Link className={styles.speedDialLink} target="_blank" rel="noopener noreferrer" href={"https://github.com/deuvarney"}>
             <GitHub />
         </Link>
     )
@@ -58,8 +50,8 @@ export default function CustomSpeedDial() {
 
         <Box className={"speedDialBtn"} sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
             <SpeedDial  
-                // open={true} 
-                // openIcon={<Launch/>}       
+                // open={true} // Dev
+                // openIcon={<Close/>}       
                 ariaLabel="SpeedDial for Contact Links"
                 sx={{ position: 'absolute', bottom: 16, right: 16 }}
                 icon={<Launch />}
