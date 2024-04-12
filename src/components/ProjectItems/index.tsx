e131d00d1f0aa7b792f371042ca626c727757cb2
deuvarney <deuvarney@yahoo.com>
2024-04-12 03:36:45 -0400

    Added fixed hight and Learn More text to Project tiles. Updated focus/hover color

--- src/components/ProjectItems/index.tsx
+++ src/components/ProjectItems/index.tsx
@@ -11,6 +11,7 @@ import styles from './styles.module.scss';
 import Link from "next/link";
 import HeaderTypography from "../HeaderTypography";
 import { WorkHistoryProject } from "@/types/types";
+import Launch from "@mui/icons-material/Launch";
 
 type WorkProjectItemCardProps = {
     name: string;
@@ -24,30 +25,33 @@ type WorkProjectItemCardProps = {
 
 function WorkProjectItemCard(props: WorkProjectItemCardProps) {
     const {name, summary, tileImg, tools, languages, renderDivider, url} = props;
-    const projectTileImg = tileImg || '/345x140.svg';
+    const projectTileImg = tileImg || '/345x140.svg'; // TODO: Move this string to a constants file
     return (
         <Grid item xs={12} sm={12} md={6} lg={6} xl={4}>
             <Card sx={{ maxWidth: 345, height: '100%', margin: 'auto'}} elevation={8}>
-                <CardActionArea>
-                    <Link href={url}>
-                        {/* TODO: This creates a link inside of a btn, look into improving this */}
-
-                        <CardMedia
-                            component="img"
-                            height="140"
-                            image={projectTileImg}
-                            alt={`${name} logo`}
-                            className={styles.cardMedia}
-                        />
-                        <CardContent>
+                <CardActionArea LinkComponent={Link} href={url} className={styles.cardActionArea}>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image={projectTileImg}
+                        alt={`${name} logo`}
+                        className={styles.cardMedia}
+                    />
+                    <CardContent className={styles.cardContent}>
                         <Typography className={styles.cardProjectName} gutterBottom variant="h5" component="div">
                             {name}
                         </Typography>
                         <Typography variant="body2" color="text.secondary">
                             {summary}
                         </Typography>
-                        </CardContent>
-                    </Link>
+                        <div className={styles.fadeEffectElement}/>
+                    </CardContent>
+
+                    <CardActions className={styles.cardActions}>
+                        <div className={styles.cardBtnLinkCont}>
+                            <span className={styles.cardBtnText}>Learn More</span><Launch/>
+                        </div>
+                    </CardActions>
                 </CardActionArea>
             </Card>
         </Grid>
